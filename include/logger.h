lkadian/c442

import files from original repo

include/logger.h
@@ -1,0 +1,41 @@
+#ifndef C442_LOGGER_H_
+#define C442_LOGGER_H_
+
+#include <memory>
+#include <vector>
+
+namespace c442 {
+
+enum class ErrorType { LEXING, SYNTAX, SEMANTIC };
+enum class WarningType { LEXING, SYNTAX, SEMANTIC };
+
+/**
+ * Logger singleton to keep track of errors and warnings
+ * across different compilation stages.
+ */
+class Logger {
+ public:
+  static void Err(const std::string& msg, ErrorType type);
+  static void Err(const std::string& msg, int line, ErrorType type);
+  static bool HasErrors();
+  static void PrintErrors();
+  static std::vector<std::string> GetErrors();
+
+  static void Warn(const std::string& msg, WarningType type);
+  static void Warn(const std::string& msg, int line, WarningType type);
+  static bool HasWarnings();
+  static void PrintWarnings();
+  static std::vector<std::string> GetWarnings();
+
+  static void Clear();
+  static std::shared_ptr<Logger> Instance();
+
+ private:
+  static std::shared_ptr<Logger> instance_;
+  std::vector<std::pair<int, std::string>> errors_;
+  std::vector<std::pair<int, std::string>> warnings_;
+};
+
+}  // namespace c442
+
+#endif  // C442_LOGGER_H_

include/mem_size_visitor.h
@@ -1,0 +1,73 @@
+#ifndef C442_MEM_SIZE_VISITOR_H_
+#define C442_MEM_SIZE_VISITOR_H_
+
+#include <unordered_map>
+
+#include "ast_visitor.h"
+
+namespace c442 {
+
+/**
+ * Visitor to traverse the AST and calculates the memory to be allocated.
+ */
+class MemSizeVisitor : public ASTVisitor {
+ public:
+  MemSizeVisitor();
+  void Visit(AParamsNode& node) override;
+  void Visit(AddOpNode& node) override;
+  void Visit(ArithExprNode& node) override;
+  void Visit(AssignNode& node) override;
+  void Visit(ClassListNode& node) override;
+  void Visit(ClassNode& node) override;
+  void Visit(DataMemberNode& node) override;
+  void Visit(DimListNode& node) override;
+  void Visit(DimNode& node) override;
+  void Visit(FParamsListNode& node) override;
+  void Visit(FParamsNode& node) override;
+  void Visit(FloatNumNode& node) override;
+  void Visit(FuncBodyNode& node) override;
+  void Visit(FuncCallNode& node) override;
+  void Visit(FuncDefListNode& node) override;
+  void Visit(FuncDefNode& node) override;
+  void Visit(IdNode& node) override;
+  void Visit(IfStatNode& node) override;
+  void Visit(IndiceList& node) override;
+  void Visit(InheritListNode& node) override;
+  void Visit(IntNumNode& node) override;
+  void Visit(MainNode& node) override;
+  void Visit(MemberFuncDeclNode& node) override;
+  void Visit(MemberListNode& node) override;
+  void Visit(MemberVarDeclNode& node) override;
+  void Visit(MultOpNode& node) override;
+  void Visit(NotNode& node) override;
+  void Visit(ProgNode& node) override;
+  void Visit(ReadNode& node) override;
+  void Visit(RelExprNode& node) override;
+  void Visit(RelOpNode& node) override;
+  void Visit(ReturnNode& node) override;
+  void Visit(ScopeResNode& node) override;
+  void Visit(SignNode& node) override;
+  void Visit(StatListNode& node) override;
+  void Visit(TypeNode& node) override;
+  void Visit(VarDeclListNode& node) override;
+  void Visit(VarDeclNode& node) override;
+  void Visit(VarNode& node) override;
+  void Visit(WhileNode& node) override;
+  void Visit(WriteNode& node) override;
+
+ private:
+  std::unordered_map<std::string, int> type_sizes_;
+  void InitTypeSizes(ProgNode& node);
+  int GetEntrySize(const Entry& entry);
+
+  // Generate a unique tempvar name
+  std::string GetTempVarName();
+  // Generate a unique literal value name
+  std::string GetLitValName();
+  int temp_var_counter;
+  int lit_val_counter;
+};
+
+}  // namespace c442
+
+#endif  // C442_MEM_SIZE_VISITOR_H_

include/token.h
@@ -1,0 +1,97 @@
+#ifndef C442_TOKEN_H_
+#define C442_TOKEN_H_
+
+#include <iostream>
+
+namespace c442 {
+
+enum class TokenType;
+
+/**
+ * Represents a token generated by the lexer.
+ */
+class Token {
+ public:
+  Token() = default;
+  Token(TokenType type, std::string lexeme, int line);
+  TokenType Type() const;
+  std::string Lexeme() const;
+  int Line() const;
+  bool IsComment() const;
+
+  friend std::ostream& operator<<(std::ostream& os, const Token& tk);
+  friend bool operator==(const Token& tk1, const Token& tk2);
+
+ private:
+  TokenType type_;
+  std::string lexeme_;
+  int line_;  // first line the token appears in the source code.
+};
+bool operator==(const Token& tk1, const Token& tk2);
+std::ostream& operator<<(std::ostream& os, const Token& tk);
+
+std::string TokenTypeToString(const TokenType& type);
+
+enum class TokenType {
+  ID,
+  INTNUM,
+  FLOATNUM,
+  EQ,
+  NEQ,
+  LT,
+  GT,
+  LEQ,
+  GEQ,
+  PLUS,
+  MINUS,
+  MULT,
+  DIV,
+  ASSGN,
+  OPEN_PAR,
+  CLOSE_PAR,
+  OPEN_CBR,
+  CLOSE_CBR,
+  OPEN_SQBR,
+  CLOSE_SQBR,
+  SEMICOLON,
+  COMMA,
+  DOT,
+  COLON,
+  SCOPE_RES,
+  // Reserved words.
+  IF,
+  THEN,
+  ELSE,
+  WHILE,
+  CLASS,
+  INTEGER,
+  FLOAT,
+  DO,
+  END,
+  PUBLIC,
+  PRIVATE,
+  OR,
+  AND,
+  NOT,
+  READ,
+  WRITE,
+  RETURN,
+  MAIN,
+  INHERITS,
+  LOCAL,
+  VOID,
+  // Comments.
+  BLOCK_CMT,
+  INLINE_CMT,
+  // Lexical errors.
+  INVALID_ID,
+  INVALID_CHAR,
+  INVALID_NUM,
+  UNTERMINATED_CMT,
+  // End of source, returned by NextToken() if there are no more tokens to scan.
+  EOS
+};
+
+}  // namespace c442
+
+#endif  // C442_TOKEN_H_

include/type_check_visitor.h
@@ -1,0 +1,55 @@
+#ifndef C442_TYPE_CHECK_VISITOR_H
+#define C442_TYPE_CHECK_VISITOR_H_
+
+#include "ast_visitor.h"
+
+namespace c442 {
+
+class TypeCheckVisitor : public ASTVisitor {
+ public:
+  void Visit(AParamsNode& node) override;
+  void Visit(AddOpNode& node) override;
+  void Visit(ArithExprNode& node) override;
+  void Visit(AssignNode& node) override;
+  void Visit(ClassListNode& node) override;
+  void Visit(ClassNode& node) override;
+  void Visit(DataMemberNode& node) override;
+  void Visit(DimListNode& node) override;
+  void Visit(DimNode& node) override;
+  void Visit(FParamsListNode& node) override;
+  void Visit(FParamsNode& node) override;
+  void Visit(FloatNumNode& node) override;
+  void Visit(FuncBodyNode& node) override;
+  void Visit(FuncCallNode& node) override;
+  void Visit(FuncDefListNode& node) override;
+  void Visit(FuncDefNode& node) override;
+  void Visit(IdNode& node) override;
+  void Visit(IfStatNode& node) override;
+  void Visit(IndiceList& node) override;
+  void Visit(InheritListNode& node) override;
+  void Visit(IntNumNode& node) override;
+  void Visit(MainNode& node) override;
+  void Visit(MemberFuncDeclNode& node) override;
+  void Visit(MemberListNode& node) override;
+  void Visit(MemberVarDeclNode& node) override;
+  void Visit(MultOpNode& node) override;
+  void Visit(NotNode& node) override;
+  void Visit(ProgNode& node) override;
+  void Visit(ReadNode& node) override;
+  void Visit(RelExprNode& node) override;
+  void Visit(RelOpNode& node) override;
+  void Visit(ReturnNode& node) override;
+  void Visit(ScopeResNode& node) override;
+  void Visit(SignNode& node) override;
+  void Visit(StatListNode& node) override;
+  void Visit(TypeNode& node) override;
+  void Visit(VarDeclListNode& node) override;
+  void Visit(VarDeclNode& node) override;
+  void Visit(VarNode& node) override;
+  void Visit(WhileNode& node) override;
+  void Visit(WriteNode& node) override;
+};
+
+}  // namespace c442
+
+#endif  // C442_TYPE_CHECK_VISITOR_H_

include/util.h
@@ -1,0 +1,47 @@
+#ifndef C442_UTIL_H_
+#define C442_UTIL_H_
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+namespace c442 {
+namespace util {
+
+// Run the generated moon code and return the output.
+std::string ExeGeneratedCode();
+
+// Trim whitespace from both ends of a string.
+std::string TrimString(const std::string& str);
+
+std::vector<std::string> SplitStrByNewline(const std::string& str);
+
+template <typename T>
+void PPrintVector(const std::vector<T> vec) {
+  std::cout << "[";
+  auto separator = ", ";
+  auto sep = "";
+  for (T val : vec) {
+    std::cout << sep << val;
+    sep = separator;
+  }
+  std::cout << "]" << std::endl;
+};
+
+template <typename T>
+void PPrintSet(const std::set<T> vec) {
+  std::cout << "{";
+  auto separator = ", ";
+  auto sep = "";
+  for (T val : vec) {
+    std::cout << sep << val;
+    sep = separator;
+  }
+  std::cout << "}" << std::endl;
+};
+
+}  // namespace util
+}  // namespace c442
+
+#endif  // C442_UTIL_H_

lkadian/moon

include/logger.h
@@ -1,0 +1,41 @@
+#ifndef TOY_LOGGER_H_
+#define TOY_LOGGER_H_
+
+#include <memory>
+#include <vector>
+
+namespace toy {
+
+enum class ErrorType { LEXING, SYNTAX, SEMANTIC };
+enum class WarningType { LEXING, SYNTAX, SEMANTIC };
+
+/**
+ * Logger singleton to keep track of errors and warnings
+ * across different compilation stages.
+ */
+class Logger {
+ public:
+  static void Err(const std::string& msg, ErrorType type);
+  static void Err(const std::string& msg, int line, ErrorType type);
+  static bool HasErrors();
+  static void PrintErrors();
+  static std::vector<std::string> GetErrors();
+
+  static void Warn(const std::string& msg, WarningType type);
+  static void Warn(const std::string& msg, int line, WarningType type);
+  static bool HasWarnings();
+  static void PrintWarnings();
+  static std::vector<std::string> GetWarnings();
+
+  static void Clear();
+  static std::shared_ptr<Logger> Instance();
+
+ private:
+  static std::shared_ptr<Logger> instance_;
+  std::vector<std::pair<int, std::string>> errors_;
+  std::vector<std::pair<int, std::string>> warnings_;
+};
+
+}  // namespace toy
+
+#endif  // TOY_LOGGER_H_

include/mem_size_visitor.h
@@ -1,0 +1,73 @@
+#ifndef TOY_MEM_SIZE_VISITOR_H_
+#define TOY_MEM_SIZE_VISITOR_H_
+
+#include <unordered_map>
+
+#include "ast_visitor.h"
+
+namespace toy {
+
+/**
+ * Visitor to traverse the AST and calculates the memory to be allocated.
+ */
+class MemSizeVisitor : public ASTVisitor {
+ public:
+  MemSizeVisitor();
+  void Visit(AParamsNode& node) override;
+  void Visit(AddOpNode& node) override;
+  void Visit(ArithExprNode& node) override;
+  void Visit(AssignNode& node) override;
+  void Visit(ClassListNode& node) override;
+  void Visit(ClassNode& node) override;
+  void Visit(DataMemberNode& node) override;
+  void Visit(DimListNode& node) override;
+  void Visit(DimNode& node) override;
+  void Visit(FParamsListNode& node) override;
+  void Visit(FParamsNode& node) override;
+  void Visit(FloatNumNode& node) override;
+  void Visit(FuncBodyNode& node) override;
+  void Visit(FuncCallNode& node) override;
+  void Visit(FuncDefListNode& node) override;
+  void Visit(FuncDefNode& node) override;
+  void Visit(IdNode& node) override;
+  void Visit(IfStatNode& node) override;
+  void Visit(IndiceList& node) override;
+  void Visit(InheritListNode& node) override;
+  void Visit(IntNumNode& node) override;
+  void Visit(MainNode& node) override;
+  void Visit(MemberFuncDeclNode& node) override;
+  void Visit(MemberListNode& node) override;
+  void Visit(MemberVarDeclNode& node) override;
+  void Visit(MultOpNode& node) override;
+  void Visit(NotNode& node) override;
+  void Visit(ProgNode& node) override;
+  void Visit(ReadNode& node) override;
+  void Visit(RelExprNode& node) override;
+  void Visit(RelOpNode& node) override;
+  void Visit(ReturnNode& node) override;
+  void Visit(ScopeResNode& node) override;
+  void Visit(SignNode& node) override;
+  void Visit(StatListNode& node) override;
+  void Visit(TypeNode& node) override;
+  void Visit(VarDeclListNode& node) override;
+  void Visit(VarDeclNode& node) override;
+  void Visit(VarNode& node) override;
+  void Visit(WhileNode& node) override;
+  void Visit(WriteNode& node) override;
+
+ private:
+  std::unordered_map<std::string, int> type_sizes_;
+  void InitTypeSizes(ProgNode& node);
+  int GetEntrySize(const Entry& entry);
+
+  // Generate a unique tempvar name
+  std::string GetTempVarName();
+  // Generate a unique literal value name
+  std::string GetLitValName();
+  int temp_var_counter;
+  int lit_val_counter;
+};
+
+}  // namespace toy
+
+#endif  // TOY_MEM_SIZE_VISITOR_H_

include/token.h
@@ -1,0 +1,97 @@
+#ifndef TOY_TOKEN_H_
+#define TOY_TOKEN_H_
+
+#include <iostream>
+
+namespace toy {
+
+enum class TokenType;
+
+/**
+ * Represents a token generated by the lexer.
+ */
+class Token {
+ public:
+  Token() = default;
+  Token(TokenType type, std::string lexeme, int line);
+  TokenType Type() const;
+  std::string Lexeme() const;
+  int Line() const;
+  bool IsComment() const;
+
+  friend std::ostream& operator<<(std::ostream& os, const Token& tk);
+  friend bool operator==(const Token& tk1, const Token& tk2);
+
+ private:
+  TokenType type_;
+  std::string lexeme_;
+  int line_;  // first line the token appears in the source code.
+};
+bool operator==(const Token& tk1, const Token& tk2);
+std::ostream& operator<<(std::ostream& os, const Token& tk);
+
+std::string TokenTypeToString(const TokenType& type);
+
+enum class TokenType {
+  ID,
+  INTNUM,
+  FLOATNUM,
+  EQ,
+  NEQ,
+  LT,
+  GT,
+  LEQ,
+  GEQ,
+  PLUS,
+  MINUS,
+  MULT,
+  DIV,
+  ASSGN,
+  OPEN_PAR,
+  CLOSE_PAR,
+  OPEN_CBR,
+  CLOSE_CBR,
+  OPEN_SQBR,
+  CLOSE_SQBR,
+  SEMICOLON,
+  COMMA,
+  DOT,
+  COLON,
+  SCOPE_RES,
+  // Reserved words.
+  IF,
+  THEN,
+  ELSE,
+  WHILE,
+  CLASS,
+  INTEGER,
+  FLOAT,
+  DO,
+  END,
+  PUBLIC,
+  PRIVATE,
+  OR,
+  AND,
+  NOT,
+  READ,
+  WRITE,
+  RETURN,
+  MAIN,
+  INHERITS,
+  LOCAL,
+  VOID,
+  // Comments.
+  BLOCK_CMT,
+  INLINE_CMT,
+  // Lexical errors.
+  INVALID_ID,
+  INVALID_CHAR,
+  INVALID_NUM,
+  UNTERMINATED_CMT,
+  // End of source, returned by NextToken() if there are no more tokens to scan.
+  EOS
+};
+
+}  // namespace toy
+
+#endif  // TOY_TOKEN_H_

include/type_check_visitor.h
@@ -1,0 +1,55 @@
+#ifndef TOY_TYPE_CHECK_VISITOR_H
+#define TOY_TYPE_CHECK_VISITOR_H_
+
+#include "ast_visitor.h"
+
+namespace toy {
+
+class TypeCheckVisitor : public ASTVisitor {
+ public:
+  void Visit(AParamsNode& node) override;
+  void Visit(AddOpNode& node) override;
+  void Visit(ArithExprNode& node) override;
+  void Visit(AssignNode& node) override;
+  void Visit(ClassListNode& node) override;
+  void Visit(ClassNode& node) override;
+  void Visit(DataMemberNode& node) override;
+  void Visit(DimListNode& node) override;
+  void Visit(DimNode& node) override;
+  void Visit(FParamsListNode& node) override;
+  void Visit(FParamsNode& node) override;
+  void Visit(FloatNumNode& node) override;
+  void Visit(FuncBodyNode& node) override;
+  void Visit(FuncCallNode& node) override;
+  void Visit(FuncDefListNode& node) override;
+  void Visit(FuncDefNode& node) override;
+  void Visit(IdNode& node) override;
+  void Visit(IfStatNode& node) override;
+  void Visit(IndiceList& node) override;
+  void Visit(InheritListNode& node) override;
+  void Visit(IntNumNode& node) override;
+  void Visit(MainNode& node) override;
+  void Visit(MemberFuncDeclNode& node) override;
+  void Visit(MemberListNode& node) override;
+  void Visit(MemberVarDeclNode& node) override;
+  void Visit(MultOpNode& node) override;
+  void Visit(NotNode& node) override;
+  void Visit(ProgNode& node) override;
+  void Visit(ReadNode& node) override;
+  void Visit(RelExprNode& node) override;
+  void Visit(RelOpNode& node) override;
+  void Visit(ReturnNode& node) override;
+  void Visit(ScopeResNode& node) override;
+  void Visit(SignNode& node) override;
+  void Visit(StatListNode& node) override;
+  void Visit(TypeNode& node) override;
+  void Visit(VarDeclListNode& node) override;
+  void Visit(VarDeclNode& node) override;
+  void Visit(VarNode& node) override;
+  void Visit(WhileNode& node) override;
+  void Visit(WriteNode& node) override;
+};
+
+}  // namespace toy
+
+#endif  // TOY_TYPE_CHECK_VISITOR_H_

include/util.h
@@ -1,0 +1,47 @@
+#ifndef TOY_UTIL_H_
+#define TOY_UTIL_H_
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+namespace toy {
+namespace util {
+
+// Run the generated moon code and return the output.
+std::string ExeGeneratedCode();
+
+// Trim whitespace from both ends of a string.
+std::string TrimString(const std::string& str);
+
+std::vector<std::string> SplitStrByNewline(const std::string& str);
+
+template <typename T>
+void PPrintVector(const std::vector<T> vec) {
+  std::cout << "[";
+  auto separator = ", ";
+  auto sep = "";
+  for (T val : vec) {
+    std::cout << sep << val;
+    sep = separator;
+  }
+  std::cout << "]" << std::endl;
+};
+
+template <typename T>
+void PPrintSet(const std::set<T> vec) {
+  std::cout << "{";
+  auto separator = ", ";
+  auto sep = "";
+  for (T val : vec) {
+    std::cout << sep << val;
+    sep = separator;
+  }
+  std::cout << "}" << std::endl;
+};
+
+}  // namespace util
+}  // namespace toy
+
+#endif  // TOY_UTIL_H_